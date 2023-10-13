b7d3020287b7c906a39bf7901ef76a06e5906a68
VanDe2003 <phanvande2003.qt@gmail.com>
2023-10-13 22:35:08 +0700

    update UI

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,103 +1,117 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { FontAwesome } from "@expo/vector-icons";
-import { auth } from "../firebaseConfig";
-import { getNameFromFirestore } from "../firebaseConfig"; 
+import React, {Component} from "react";
+import {View, Text, StyleSheet, TouchableOpacity, Image} from "react-native";
+import {FontAwesome} from "@expo/vector-icons";
+import {auth} from "../firebaseConfig";
+import {getNameFromFirestore} from "../firebaseConfig";
 class HomeScreen extends Component {
-  state = {
-    name: "",
-  };
+    state = {
+        name: "",
+    };
 
-  async componentDidMount() {
-    const user = auth.currentUser;
-    if (user) {
-      const name = await getNameFromFirestore(user.email);
-      if (name) {
-        this.setState({ name });
-      }
+    async componentDidMount() {
+        const user = auth.currentUser;
+        if (user) {
+            const name = await getNameFromFirestore(user.email);
+            if (name) {
+                this.setState({name});
+            }
+        }
     }
-  }
-  handleHomeButton = () => {
-    this.props.navigation.navigate("Home");
-  };
+    handleHomeButton = () => {
+        this.props.navigation.navigate("Home");
+    };
 
-  handleProfileButton = () => {
-    this.props.navigation.navigate("Profile");
-  };
+    handleProfileButton = () => {
+        this.props.navigation.navigate("Profile");
+    };
 
-  handleLogoutButton = () => {
-    this.props.navigation.navigate("Login");
-  };
-  handleScannerButton = () => {
-    this.props.navigation.navigate("QRCodeScanner");
-  };
+    handleLogoutButton = () => {
+        this.props.navigation.navigate("Login");
+    };
+    handleScannerButton = () => {
+        this.props.navigation.navigate("QRCodeScanner");
+    };
 
-  render() {
-    const { name } = this.state;
-    return (
-      <View style={styles.container}>
-        <Text>Xin chào {name}</Text>
-        <Text style={styles.title}>Trang chủ</Text>
+    render() {
+        const {name} = this.state;
+        return (
+            <View style={styles.container}>
+                {/* <Text style={styles.title}>Trang chủ</Text> */}
+                <View style={styles.header}>
+                    <Image source={require("../images/logo.png")} style={styles.image} />
+                    <Text>Xin chào {name}</Text>
+                </View>
 
-        <View style={styles.bottomButtons}>
-          <TouchableOpacity
-            style={styles.bottomButton}
-            onPress={this.handleHomeButton}
-          >
-            <FontAwesome name="home" size={24} color="black" />
-            <Text style={styles.bottomButtonText}>Trang Chủ</Text>
-          </TouchableOpacity>
+                <View style={styles.bottomButtons}>
+                    <TouchableOpacity style={styles.bottomButton} onPress={this.handleHomeButton}>
+                        <FontAwesome name="home" size={24} color="black" />
+                        {/* <Text style={styles.bottomButtonText}>Trang Chủ</Text> */}
+                    </TouchableOpacity>
 
-          <TouchableOpacity
-            style={styles.bottomButton}
-            onPress={this.handleProfileButton}
-          >
-            <FontAwesome name="user" size={24} color="black" />
-            <Text style={styles.bottomButtonText}>Thông Tin Cá Nhân</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.bottomButton}
-            onPress={this.handleScannerButton}
-          >
-            <FontAwesome name="camera" size={24} color="black" />
-            <Text style={styles.bottomButtonText}>Quét QR</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.bottomButton}
-            onPress={this.handleLogoutButton}
-          >
-            <FontAwesome name="sign-out" size={24} color="black" />
-            <Text style={styles.bottomButtonText}>Đăng Xuất</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-  }
+                    <TouchableOpacity style={styles.bottomButton} onPress={this.handleProfileButton}>
+                        <FontAwesome name="user" size={24} color="black" />
+                        {/* <Text style={styles.bottomButtonText}>Thông Tin Cá Nhân</Text> */}
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.bottomButton} onPress={this.handleScannerButton}>
+                        <FontAwesome name="camera" size={24} color="black" />
+                        {/* <Text style={styles.bottomButtonText}>Quét QR</Text> */}
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.bottomButton} onPress={this.handleLogoutButton}>
+                        <FontAwesome name="sign-out" size={24} color="black" />
+                        {/* <Text style={styles.bottomButtonText}>Đăng Xuất</Text> */}
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f4f4f4",
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-  bottomButtons: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "100%",
-    paddingHorizontal: 20,
-  },
-  bottomButton: {
-    alignItems: "center",
-  },
-  bottomButtonText: {
-    marginTop: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F1F2F6",
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 20,
+    },
+    bottomButtons: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "90%",
+        paddingHorizontal: 20,
+        backgroundColor: "#C2C2C2",
+        padding: 20,
+        borderRadius: 100,
+        position: "absolute",
+        bottom: "5%",
+    },
+    bottomButton: {
+        alignItems: "center",
+    },
+    bottomButtonText: {
+        marginTop: 5,
+    },
+
+    header: {
+        position: "absolute",
+        top: "5%",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: "#C7EEE0",
+        width: "90%",
+        padding: 20,
+        borderRadius: 50,
+    },
+
+    image: {
+        width: 90,
+        height: 90,
+        marginRight: 25,
+    },
 });
 
 export default HomeScreen;
--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -1,166 +1,188 @@
-import React, { Component } from 'react';
-import { View, Text,StyleSheet,TouchableOpacity } from 'react-native';
-import QRCode from 'react-native-qrcode-svg';
-import { getNameFromFirestore } from '../firebaseConfig'; // Import the function
-import { auth } from '../firebaseConfig'; // Import Firebase authentication
-import { getPhoneFromFirestore } from '../firebaseConfig';
+import React, {Component} from "react";
+import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
+import QRCode from "react-native-qrcode-svg";
+import {getNameFromFirestore} from "../firebaseConfig"; // Import the function
+import {auth} from "../firebaseConfig"; // Import Firebase authentication
+import {getPhoneFromFirestore} from "../firebaseConfig";
 
 class ProfileScreen extends Component {
-  state = {
-    name: '',
-    email: '',
-    phone: '',
-    qrData: '',
-  };
+    state = {
+        name: "",
+        email: "",
+        phone: "",
+        qrData: "",
+    };
 
-  async componentDidMount() {
-    const user = auth.currentUser;
-    if (user) {
-      const { email,phone } = user;
+    async componentDidMount() {
+        const user = auth.currentUser;
+        if (user) {
+            const {email, phone} = user;
 
-      try {
-       const phone=await getPhoneFromFirestore(email);
-        const name = await getNameFromFirestore(email);
-        if (name) {
-          this.setState({ name });
+            try {
+                const phone = await getPhoneFromFirestore(email);
+                const name = await getNameFromFirestore(email);
+                if (name) {
+                    this.setState({name});
+                }
+                if (email) {
+                    this.setState({email});
+                }
+                if (phone) {
+                    this.setState({phone});
+                }
+            } catch (error) {
+                console.error("Error fetching user info:", error);
+            }
         }
-        if(email){this.setState({email})};
-        if(phone){this.setState({phone})};
-    
-      } catch (error) {
-        console.error('Error fetching user info:', error);
-      }
-      
     }
-    
-  }
-  handleViewFriendList = () => {
-    // Navigate to FriendList screen
-    // You should have a navigation object from react-navigation or similar to navigate
-    this.props.navigation.navigate('FriendList');
-  };
-  render() {
-    const { name, email, phone} = this.state;
-    const qrData = ` ${name}, ${email}, ${phone}`;
-    return (
-      <View style={styles.container}>
-        <View style={styles.profileHeader}>
-          <Text style={styles.headerText}>Thông tin cá nhân</Text>
-        </View>
-        <View style={styles.profileInfoContainer}>
-          <Text style={styles.label}>Họ và Tên:</Text>
-          <Text style={styles.value}>{name}</Text>
-          <Text style={styles.label}>Email:</Text>
-          <Text style={styles.value}>{email}</Text>
-          <Text style={styles.label}>Số điện thoại:</Text>
-          <Text style={styles.value}>{phone}</Text>
-
-        </View>
-        <TouchableOpacity onPress={this.handleViewFriendList} style={styles.viewFriendListButton}>
-          <Text style={styles.viewFriendListButtonText}>Xem danh sách bạn bè</Text>
-        </TouchableOpacity>
-          <View style={styles.qrCodeContainer}>
-          <QRCode value={qrData} size={200} />
-        </View> 
-      </View>
-    );
-  }
+    handleViewFriendList = () => {
+        this.props.navigation.navigate("FriendList");
+    };
+    render() {
+        const {name, email, phone} = this.state;
+        const qrData = ` ${name}, ${email}, ${phone}`;
+        return (
+            <View style={styles.container}>
+                <View style={styles.profileHeader}>
+                    <Text style={styles.headerText}>Thông tin cá nhân</Text>
+                </View>
+                <View style={styles.infoContainer}>
+                    <View style={styles.profileInfoContainer}>
+                        <Text style={styles.label}>Họ tên: {name}</Text>
+                        {/* <Text style={styles.value}>{name}</Text> */}
+                        <Text style={styles.label}>Email: {email}</Text>
+                        {/* <Text style={styles.value}>{email}</Text> */}
+                        <Text style={styles.label}>Số điện thoại: {phone}</Text>
+                        {/* <Text style={styles.value}>{phone}</Text> */}
+                    </View>
+                    <TouchableOpacity onPress={this.handleViewFriendList} style={styles.viewFriendListButton}>
+                        <Text style={styles.viewFriendListButtonText}>Danh sách bạn bè</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.qrCodeContainer}>
+                    <QRCode value={qrData} size={200} backgroundColor="#F1F2F6" />
+                </View>
+            </View>
+        );
+    }
 }
 
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f0f2f5",
-    padding: 20,
-  },
-  profileHeader: {
-    backgroundColor: "blue",
-    padding: 10,
-    marginBottom: 20,
-  },
-  headerText: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "white",
-  },
-  editProfileContainer: {
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
+    container: {
+        flex: 1,
+        backgroundColor: "#F1F2F6",
+        padding: 40,
     },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  input: {
-    marginBottom: 10,
-    padding: 10,
-    borderColor: "#ccc",
-    borderWidth: 1,
-    borderRadius: 5,
-  },
-  saveButton: {
-    backgroundColor: "#1877f2",
-    padding: 10,
-    borderRadius: 5,
-    alignItems: "center",
-  },
-  saveButtonText: {
-    color: "white",
-    fontWeight: "bold",
-  },
-  viewProfileContainer: {
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
+    profileHeader: {
+        backgroundColor: "#C7EEE0",
+        padding: 10,
+        marginBottom: 10,
+        alignItems: "center",
+        borderRadius: 6,
     },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  label: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  value: {
-    fontSize: 16,
-    marginBottom: 10,
-  },
-  editButton: {
-    backgroundColor: "blue",
-    padding: 10,
-    borderRadius: 5,
-    alignItems: "center",
-    marginTop: 10,
-  },
-  editButtonText: {
-    color: "white",
-    fontWeight: "bold",
-  },
-  viewFriendsButton: {
-    backgroundColor: "#4caf50",
-    padding: 10,
-    borderRadius: 5,
-    alignItems: "center",
-    marginTop: 10,
-  },
-  viewFriendsButtonText: {
-    color: "white",
-    fontWeight: "bold",
-  },
-  qrCodeContainer: {
-    alignItems: "center",
-    marginTop: 20,
-  },
+    headerText: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "black",
+    },
+    editProfileContainer: {
+        backgroundColor: "white",
+        padding: 20,
+        borderRadius: 10,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    input: {
+        marginBottom: 10,
+        padding: 10,
+        borderColor: "#ccc",
+        borderWidth: 1,
+        borderRadius: 5,
+    },
+    saveButton: {
+        backgroundColor: "#1877f2",
+        padding: 10,
+        borderRadius: 5,
+        alignItems: "center",
+    },
+    saveButtonText: {
+        color: "white",
+        fontWeight: "bold",
+    },
+    viewProfileContainer: {
+        backgroundColor: "white",
+        padding: 20,
+        borderRadius: 10,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    label: {
+        fontSize: 16,
+        fontWeight: "bold",
+        marginBottom: 10,
+    },
+    // value: {
+    //     fontSize: 16,
+    //     marginBottom: 10,
+    // },
+    editButton: {
+        backgroundColor: "blue",
+        padding: 10,
+        borderRadius: 5,
+        alignItems: "center",
+        marginTop: 10,
+    },
+    editButtonText: {
+        color: "white",
+        fontWeight: "bold",
+    },
+    viewFriendsButton: {
+        backgroundColor: "#4caf50",
+        padding: 10,
+        borderRadius: 5,
+        alignItems: "center",
+        marginTop: 10,
+    },
+    viewFriendsButtonText: {
+        color: "white",
+        fontWeight: "bold",
+    },
+    qrCodeContainer: {
+        alignItems: "center",
+        marginTop: 20,
+    },
+    viewFriendListButtonText: {
+        color: "black",
+        fontSize: 19,
+        fontWeight: "bold",
+        padding: 10,
+    },
+
+    viewFriendListButton: {
+        backgroundColor: "#C7EEE0",
+        display: "flex",
+        alignItems: "center",
+        borderRadius: 6,
+    },
+    infoContainer: {
+        backgroundColor: "white",
+        padding: 40,
+        borderRadius: 6,
+    },
+
+    profileInfoContainer: {paddingBottom: 20},
 });
 
 export default ProfileScreen;
